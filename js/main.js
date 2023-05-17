f00581e73ae5bacd1bc9cd8fcd6493c29988aa92
Ogurchik0909 <ogurchik082007@gmail.com>
2023-05-17 20:17:28 +0500

    js_2dars kodlari

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/*
+if (agar)
+else (u holda)
+else if (yoki bo'lmasa)
+*/
+/* number,string */
+/*if (45 == 45){
+    document.write("Ular teng!");
+}else{
+    document.write("Ular teng emas!");
+}*/
+/*let a = 46;
+let b = 45;
+if (a > b){
+    document.write("1-solishtirish bajarildi!");
+}
+else if(47==45){
+    document.write("2-solishtirish teng!");
+}
+else{
+    document.write("Yuqoridagilarning hech biri teng emas!");
+}*/
+/*let a = 10;
+let b = 19;
+let c = 46;
+let d = 49;*/
+/*if (a == 11 && b == 19 && c == 46 && d == 49){
+    document.write("Shartlarning hammasi bajarildi!");
+}
+else{
+    document.write("Shartlar to'liq bajarilmagan!");
+}*/
+/*if (a == 10 || b == 19 || c == 46 || d == 49){
+    document.write("Shartlarning hammasi bajarildi!");
+}
+else{
+    document.write("Shartlarning kamida bittasi bajarilgan!")
+}*/
+
+/* JS data types: undefined,boolean */
+
+/*let soz;
+document.write(typeof soz);*/
+
+let rost = 45 == 44 ? true:false;
+
+document.write(rost);
+
+
+
+
+
+
+
+
